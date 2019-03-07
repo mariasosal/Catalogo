85eea2a5e9d9c66208f19bd604703e6ea6fb4e91
Maria Sosa <mariasosal14@gmail.com>
2019-03-07 10:01:28 -0500

    Funciones para que las compras se vean en la factura, se detiene el programa. Revisar. Fragmento con la estructura de la factura listo.

--- app/src/main/java/com/example/lenovo/LAPOLA/Principal.java
+++ app/src/main/java/com/example/lenovo/LAPOLA/Principal.java
@@ -6,6 +6,9 @@ import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.View;
 import android.arch.persistence.room.Room;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.lenovo.catalogo.R;
 
@@ -23,6 +26,7 @@ public class Principal extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+        
 
         //Fuente
       /*  String fuente1="/fuentes/titulo.otf";
@@ -37,16 +41,16 @@ public class Principal extends Activity {
         texto2=(TextView) findViewById(R.id.txtUsu1);
         texto2.setTypeface(botones);*/
 
-
+//Base de datos
         miDatabase  = Room.databaseBuilder(getApplicationContext(),DBUsuario.class,"ubicaciondb").allowMainThreadQueries().build();
-
+//Abrir el fragmento principal al abrir la aplicacion
         FragmentManager miManejador=getFragmentManager();
         FragmentTransaction miTransaccion =miManejador.beginTransaction();
         FragPrincipal miFragmento=new FragPrincipal();
         miTransaccion.replace(R.id.contAct,miFragmento);
         miTransaccion.commit();
 
-
+//Cambiar de fragmentos
     }
     public void agregarFragUsu (View v) {
         FragmentManager miManejador=getFragmentManager();
@@ -90,13 +94,6 @@ public class Principal extends Activity {
         miTransaccion.replace(R.id.contAct,miFragmento);
         miTransaccion.commit();
     }
-    public void comprar (View v) {
-        FragmentManager miManejador=getFragmentManager();
-        FragmentTransaction miTransaccion =miManejador.beginTransaction();
-        factura miFragmento=new factura();
-        miTransaccion.replace(R.id.contAct,miFragmento);
-        miTransaccion.commit();
-    }
     public void devolver (View v) {
         FragmentManager miManejador=getFragmentManager();
         FragmentTransaction miTransaccion =miManejador.beginTransaction();
@@ -111,7 +108,7 @@ public class Principal extends Activity {
         FragUsu miFragmento=new FragUsu();
         miTransaccion.replace(R.id.contAct,miFragmento);
         miTransaccion.commit();
-        }
+    }
     public void atrasSis (View v) {
         FragmentManager miManejador=getFragmentManager();
         FragmentTransaction miTransaccion =miManejador.beginTransaction();
@@ -120,6 +117,18 @@ public class Principal extends Activity {
         miTransaccion.commit();
     }
 
+    public void comprar (View v) {
+        FragmentManager miManejador=getFragmentManager();
+        FragmentTransaction miTransaccion =miManejador.beginTransaction();
+        factura miFragmento=new factura();
+        miTransaccion.replace(R.id.contAct,miFragmento);
+        miTransaccion.commit();
+
+    }
+
+
+    //Guardar datos en la base de datos
+
     public void nuevoUsuario(View v){
         FragmentManager miManejador=getFragmentManager();
         FragmentTransaction miTransaccion =miManejador.beginTransaction();
@@ -127,13 +136,70 @@ public class Principal extends Activity {
         miNuevoUsuario=new agregarUsu();
         miTransaccion.replace(R.id.contAct,miNuevoUsuario);
         miTransaccion.commit();
-
     }
+    //Tomar datos de la compra y mostrarlos en la factura
+    public void facturar (View v) {
+        EditText miCant1 = (EditText) findViewById(R.id.cantidad1);
+        Button miCompra1=(Button) findViewById(R.id.facturar);
+        TextView mi1=(TextView) findViewById(R.id.Cant1);
+        int compra1 = Integer.parseInt(String.valueOf(miCant1.getText()));
+        int r1=compra1;
+        mi1.setText(r1);
+/*
+        EditText miCant2 = (EditText) findViewById(R.id.cantidad2);
+        Button miCompra2=(Button) findViewById(R.id.facturar);
+        TextView mi2=(TextView) findViewById(R.id.Cant2);
+        int compra2 = Integer.parseInt(String.valueOf(miCant2.getText()));
+        int r2=compra2;
+        mi2.setText(r2);
 
+        EditText miCant3 = (EditText) findViewById(R.id.cantidad3);
+        Button miCompra3=(Button) findViewById(R.id.facturar);
+        TextView mi3=(TextView) findViewById(R.id.Cant3);
+        int compra3 = Integer.parseInt(String.valueOf(miCant3.getText()));
+        int r3=compra3;
+        mi3.setText(r3);
 
+        EditText miCant4 = (EditText) findViewById(R.id.cantidad4);
+        Button miCompra4=(Button) findViewById(R.id.facturar);
+        TextView mi4=(TextView) findViewById(R.id.Cant4);
+        int compra4 = Integer.parseInt(String.valueOf(miCant4.getText()));
+        int r4=compra4;
+        mi4.setText(r4);
 
+        EditText miCant5 = (EditText) findViewById(R.id.cantidad5);
+        Button miCompra5=(Button) findViewById(R.id.facturar);
+        TextView mi5=(TextView) findViewById(R.id.Cant5);
+        int compra5 = Integer.parseInt(String.valueOf(miCant5.getText()));
+        int r5=compra5;
+        mi5.setText(r5);
 
+        EditText miCant6 = (EditText) findViewById(R.id.cantidad6);
+        Button miCompra6=(Button) findViewById(R.id.facturar);
+        TextView mi6=(TextView) findViewById(R.id.Cant6);
+        int compra6 = Integer.parseInt(String.valueOf(miCant6.getText()));
+        int r6=compra6;
+        mi6.setText(r6);
 
+        EditText miCant7 = (EditText) findViewById(R.id.cantidad7);
+        Button miCompra7=(Button) findViewById(R.id.facturar);
+        TextView mi7=(TextView) findViewById(R.id.Cant7);
+        int compra7 = Integer.parseInt(String.valueOf(miCant7.getText()));
+        int r7=compra7;
+        mi7.setText(r7);
 
+        EditText miCant8 = (EditText) findViewById(R.id.cantidad8);
+        Button miCompra8=(Button) findViewById(R.id.facturar);
+        TextView mi8=(TextView) findViewById(R.id.Cant8);
+        int compra8 = Integer.parseInt(String.valueOf(miCant8.getText()));
+        int r8=compra8;
+        mi8.setText(r8);*/
+
+        //Valor total de la compra
+        TextView total=(TextView) findViewById(R.id.total);
+         int Vltotal = (r1)*6000;
+         total.setText(Vltotal+"COP");
+//+r2+r3+r4+r5+r6+r7+r8
+    }
 
 }
